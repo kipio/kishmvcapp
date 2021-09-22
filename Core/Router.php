165dde6ab11da912a2e73ebd023ff414d76b6692
Kish <test.pionero@gmail.com>
2021-09-22 20:47:11 +0530

    routing complete

--- Core/Router.php
+++ Core/Router.php
@@ -5,8 +5,12 @@ class Router
     protected $routes = [];
     protected $params = [];
 
-    public function add($route,$params)
+    public function add($route,$params = [])
     {
+        $route = preg_replace('/\//','\\/',$route);
+        $route = preg_replace('/\{([a-z]+)\}/','(?P<\1>[a-z-]+)',$route);
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/','(?P<\1>\2)',$route);
+        $route = '/^' . $route . '$/i';
         $this->routes[$route] = $params;
     }
 
@@ -17,12 +21,32 @@ class Router
 
     public function match($url)
     {
+        /*
         foreach($this->routes as $route => $params) {
             if($url == $route) {
                 $this->params = $params;
                 return true;
             }
         }
+        */
+
+        // $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+        foreach($this->routes as $route => $params) {
+            // if(preg_match($reg_exp,$url,$matches)) {
+            if(preg_match($route,$url,$matches)) {
+                // $params = [];
+
+                foreach($matches as $key => $match ) {
+                    if(is_string($key)) {
+                        $params[$key] = $match;
+                    }
+                }
+                $this->params = $params;
+                return true;
+            }
+        }
+
         return false;
     }
 
--- public/index.php
+++ public/index.php
@@ -10,7 +10,10 @@ $router = new Router();
 
 $router->add('',['controller'=>'Home','action'=>'index']);
 $router->add('posts',['controller'=>'Posts','action'=>'index']);
-$router->add('posts/new',['controller'=>'Posts','action'=>'new']);
+// $router->add('posts/new',['controller'=>'Posts','action'=>'new']);
+$router->add('{controller}/{action}');
+//$router->add('admin/{action}/{controller}');
+$router->add('{controller}/{id:\d+}/{action}');
 
 // echo '<pre>';
 // var_dump($router->getRoutes());
